e17069e2780e068c299a59bc5bdd5134ac8383dd
veldymanov <veldymanov@gmail.com>
2017-03-17 10:39:03 +0200

    WWOC

---
+++ script/main.js
@@ -0,0 +1,7 @@
+//var conditional = document.querySelector('.conditional');
+var testElem = document.createElement('p');
+if(testElem.style.flex !== undefined && testElem.style.flexFlow !== undefined) {
+//  conditional.setAttribute('href', 'style/flex-layout.css');
+} else {
+//  conditional.setAttribute('href', 'style/float-layout.css');
+}
