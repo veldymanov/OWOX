8701459ac1407e7bdfd0e4fe4c3755dd1189d077
veldymanov <veldymanov@gmail.com>
2017-03-18 10:27:52 +0200

    Update

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
+//--------------Checking for property-----------------------------------------
 //var conditional = document.querySelector('.conditional');
 var testElem = document.createElement('p');
 if(testElem.style.flex !== undefined && testElem.style.flexFlow !== undefined) {
@@ -5,3 +6,35 @@ if(testElem.style.flex !== undefined && testElem.style.flexFlow !== undefined) {
 } else {
 //  conditional.setAttribute('href', 'style/float-layout.css');
 }
+
+//-----------------------------------info-box-----------------------------------------
+
+var tabs = document.querySelectorAll('.info-box li a');
+var panels = document.querySelectorAll('.info-box .panels div');
+
+
+for(let i = 0, l = tabs.length; i < l; i++) { 
+  var tab = tabs[i];
+  setTabHandler(tab, i);
+}
+
+function setTabHandler(tab, tabPos) {
+  tab.onclick = function() {
+    for(let i = 0, l = tabs.length; i < l; i++) {
+      if(tabs[i].getAttribute('class')) {
+        tabs[i].removeAttribute('class');
+      }
+    }
+
+    tab.setAttribute('class', 'active');
+
+    for(let i = 0, l = tabs.length; i < l; i++) {
+      if(panels[i].getAttribute('class')) {
+        panels[i].removeAttribute('class');
+      }
+    }
+	
+    panels[tabPos].setAttribute('class', 'active-panel');
+  }
+}
+//--------------------------------------------------------------------------------
