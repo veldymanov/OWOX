428b9c6119fdc22c16f9e225422bfa760a752b66
veldymanov <veldymanov@gmail.com>
2017-03-29 17:54:12 +0300

    Update

--- script/main.js
+++ script/main.js
@@ -1,35 +1,50 @@
-//--------------Checking for property-----------------------------------------
-//var conditional = document.querySelector('.conditional');
-var testElem = document.createElement('p');
-if(testElem.style.flex !== undefined && testElem.style.flexFlow !== undefined) {
-//  conditional.setAttribute('href', 'style/flex-layout.css');
-} else {
-//  conditional.setAttribute('href', 'style/float-layout.css');
+//Mobile side-menu
+document.querySelectorAll('.menuOpenBtn a')[0].addEventListener("click", openMobileMenu);
+document.querySelectorAll('.menuCloseBtn a')[0].addEventListener("click", closeMobileMenu);
+window.addEventListener("click", function(event){
+	var ev1 = !event.target.matches('.dropnav:first-child li a');
+	var ev2 = !event.target.matches('.dropnav:first-child');
+	var ev3 = !event.target.matches('.menuOpenBtn a');
+	if (ev1 && ev2 && ev3) {
+		closeMobileMenu();
+	}
+});
+
+
+function openMobileMenu(){ 
+	var mobileMenu = document.querySelectorAll('.dropnav:first-child');
+	mobileMenu[0].style.display = "block";
+}
+
+function closeMobileMenu(){
+	var mobileMenu = document.querySelectorAll('.dropnav:first-child');
+	mobileMenu[0].style.display = "none";
+	
 }
 
 //-----------------------------------info-box-----------------------------------------
-var tabs = document.querySelectorAll('.info-box li a');
-var panels = document.querySelectorAll('.info-box .panels div');
+var tabs = document.querySelectorAll('.info-box>ul li a');
+var panels = document.querySelectorAll('.info-box .panels>div');
 
 
 for(let i = 0, l = tabs.length; i < l; i++) { 
   let tab = tabs[i];
-  setTabHandler(tab, i, 'active', 'active-panel', tabs, panels);
+  setTabHandler(tab, i, tabs, panels);
 }
 
 //-----------------------------------sales-box-----------------------------------------
-var tabs1 = document.querySelectorAll('.sales-box li a');
-var panels1 = document.querySelectorAll('.sales-box .sales-panels div');
+var tabs1 = document.querySelectorAll('.sales-section li a');
+var panels1 = document.querySelectorAll('.sales-section .sales-panels div');
 
 
 for(let i = 0, l = tabs1.length; i < l; i++) { 
   let tab = tabs1[i];
-  setTabHandler(tab, i, 'active-sales', 'active-sales-panel', tabs1, panels1);
+  setTabHandler(tab, i, tabs1, panels1);
 }
 
 //-------------------------------------------------------------------------------------
 
-function setTabHandler(tab, tabPos, active, activePanel, tabs$, panels$) { 
+function setTabHandler(tab, tabPos, tabs$, panels$) { 
   tab.onclick = function() {
     for(let i = 0, l = tabs$.length; i < l; i++) {
       if(tabs$[i].getAttribute('class')) {
@@ -37,7 +52,7 @@ function setTabHandler(tab, tabPos, active, activePanel, tabs$, panels$) {
       }
     }
 
-    tab.setAttribute('class', active);
+    tab.setAttribute('class', 'active');
 
     for(let i = 0, l = panels$.length; i < l; i++) {
       if(panels$[i].getAttribute('class')) {
@@ -45,7 +60,7 @@ function setTabHandler(tab, tabPos, active, activePanel, tabs$, panels$) {
       }
     }
 	
-    panels$[tabPos].setAttribute('class', activePanel);
+    panels$[tabPos].setAttribute('class', 'active-panel');
   }
 }
 //--------------------------------------------------------------------------------
